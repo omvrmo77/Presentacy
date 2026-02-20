88079973a204bc46bbbf9522faeb7a5cd8459698
omvrmo77 <145567996+omvrmo77@users.noreply.github.com>
2026-02-21 01:41:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1548,6 +1548,7 @@ document.addEventListener("DOMContentLoaded", () => {
       `;
     };
 
+
     const renderMultipleResults = (list, term, selectedClassLabel) => {
       const cards = list
         .map(
